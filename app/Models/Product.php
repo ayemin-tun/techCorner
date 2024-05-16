1b9ea1363ce83aa6514b887a3b44f07999ca9292
aye-min-tun <ayemintun.mhs@gmail.com>
2024-05-16 16:08:46 +0630

    add migration and model for associated tables

---
+++ app/Models/Product.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'brand_id',
+        'name',
+        'slug',
+        'images',
+        'description',
+        'price',
+        'is_active',
+        'is_featured',
+        'in_stock',
+        'on_sale',
+    ];
+
+    protected $casts = [
+        'images' => 'array',
+    ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function brand(): BelongsTo
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    public function orderItems(): BelongsTo
+    {
+        return $this->belongsTo(OrderItem::class);
+    }
+}

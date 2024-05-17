2b17ca5e9d812ec8bcad9adf531f2b0d1efdaf1e
aye-min-tun <ayemintun.mhs@gmail.com>
2024-05-18 00:08:56 +0630

    add order resource need to add table (not completed)

---
+++ app/Filament/Resources/OrderResource.php
@@ -0,0 +1,190 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\OrderResource\Pages;
+use App\Models\Order;
+use App\Models\Product;
+use Filament\Forms\Components\Group;
+use Filament\Forms\Components\Hidden;
+use Filament\Forms\Components\Placeholder;
+use Filament\Forms\Components\Repeater;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\ToggleButtons;
+use Filament\Forms\Form;
+use Filament\Forms\Get;
+use Filament\Forms\Set;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\CreateAction;
+use Filament\Tables\Table;
+use Illuminate\Support\Number;
+
+class OrderResource extends Resource
+{
+    protected static ?string $model = Order::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-shopping-bag';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Group::make()->schema([
+                    Section::make('Order Information')->schema([
+                        Select::make('user_id')
+                            ->label('Customer')
+                            ->required()
+                            ->searchable()
+                            ->preload()
+                            ->relationship('user', 'name'),
+                        Select::make('payment_method')
+                            ->options([
+                                'pre_pay' => 'Pre Pay',
+                                'cod' => 'Cash On Delievery',
+                            ]),
+                        Select::make('payment_status')
+                            ->options([
+                                'pending' => 'Pending',
+                                'paid' => 'Paid',
+                                'failed' => 'failed',
+                            ]),
+                        ToggleButtons::make('status')
+                            ->required()
+                            ->options([
+                                'new' => 'New',
+                                'processing' => 'Processing',
+                                'shipped' => 'Shipped',
+                                'delivered' => 'Delivered',
+                                'cancelled' => 'Cancelled',
+                            ])
+                            ->default('new')
+                            ->colors([
+                                'new' => 'info',
+                                'processing' => 'warning',
+                                'shipped' => 'success',
+                                'delivered' => 'success',
+                                'cancelled' => 'danger',
+                            ])
+                            ->icons([
+                                'new' => 'heroicon-m-sparkles',
+                                'processing' => 'heroicon-m-arrow-path',
+                                'shipped' => 'heroicon-m-truck',
+                                'delivered' => 'heroicon-m-check-badge',
+                                'cancelled' => 'heroicon-m-x-circle',
+                            ])->inline(),
+                        Select::make('currency')
+                            ->options([
+                                'usd' => 'USD',
+                                'mmk' => 'MMK',
+                            ])
+                            ->required()
+                            ->default('usd'),
+                        Select::make('shipping_method')
+                            ->options([
+                                'royal_express' => 'Royal Express',
+                                'ninja_van' => 'Ninja_van',
+                            ]),
+                        Textarea::make('notes')
+                            ->columnSpanFull(),
+                    ])->columns(2),
+
+                    Section::make('Order Items')->schema([
+                        Repeater::make('items')
+                            ->relationship()
+                            ->schema([
+                                Select::make('product_id')
+                                    ->relationship('product', 'name')
+                                    ->searchable()
+                                    ->preload()
+                                    ->required()
+                                    ->distinct()
+                                    ->disableOptionsWhenSelectedInSiblingRepeaterItems()
+                                    ->reactive()
+                                    ->afterStateUpdated(fn ($state, Set $set) => $set('unit_amount', Product::find($state)?->price ?? 0))
+                                    ->afterStateUpdated(fn ($state, Set $set) => $set('total_amount', Product::find($state)?->price ?? 0)),
+                                TextInput::make('quantity')
+                                    ->numeric()
+                                    ->required()
+                                    ->default(1)
+                                    ->minValue(1)
+                                    ->reactive()
+                                    ->afterStateUpdated(fn ($state, Set $set, Get $get) => $set('total_amount', $state * $get('unit_amount'))),
+                                TextInput::make('unit_amount')
+                                    ->numeric()
+                                    ->required()
+                                    ->disabled()
+                                    ->dehydrated(),
+                                TextInput::make('total_amount')
+                                    ->numeric()
+                                    ->disabled()
+                                    ->dehydrated()
+                                    ->required(),
+
+                            ])->columns(4),
+                        Placeholder::make('grand_total_placeholder')
+                            ->label('Grand Total')
+                            ->content(function (Get $get, Set $set) {
+                                $total = 0;
+                                if (! $repeaters = $get('items')) {
+                                    return $total;
+                                }
+                                foreach ($repeaters as $repeater) {
+                                    // Assuming 'total_amount' is a property of each item
+                                    $total += $repeater['total_amount'];
+                                }
+                                $set('grand_total', $total);
+
+                                return Number::currency($total, 'USD');
+                            }),
+                        Hidden::make('grand_total')
+                            ->default(0),
+                    ]),
+
+                ])->columnSpanFull(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->emptyStateActions([
+                CreateAction::make(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListOrders::route('/'),
+            'create' => Pages\CreateOrder::route('/create'),
+            'view' => Pages\ViewOrder::route('/{record}'),
+            'edit' => Pages\EditOrder::route('/{record}/edit'),
+        ];
+    }
+}

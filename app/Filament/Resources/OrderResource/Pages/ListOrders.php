e11d32620ca913a286b2d742d934d83bbd4a7c37
aye-min-tun <ayemintun.mhs@gmail.com>
2024-05-18 21:02:11 +0630

    add widget and tab in Order resource

--- app/Filament/Resources/OrderResource/Pages/ListOrders.php
+++ app/Filament/Resources/OrderResource/Pages/ListOrders.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\OrderResource\Pages;
 
 use App\Filament\Resources\OrderResource;
+use App\Filament\Resources\OrderResource\Widgets\OrderStatus;
 use Filament\Actions;
+use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ListRecords;
 
 class ListOrders extends ListRecords
@@ -14,6 +16,30 @@ class ListOrders extends ListRecords
     {
         return [
             Actions\CreateAction::make(),
+        ];
+    }
+
+    protected function getHeaderWidgets(): array
+    {
+        return [
+            OrderStatus::class,
+        ];
+    }
+
+    public function getTabs(): array
+    {
+        return [
+            null => Tab::make('All'),
+            'New' => Tab::make()->query(fn ($query) => $query->where('status', 'new'))
+                ->icon('heroicon-m-sparkles'),
+            'Processing' => Tab::make()->query(fn ($query) => $query->where('status', 'processing'))
+                ->icon('heroicon-m-arrow-path'),
+            'Shipped' => Tab::make()->query(fn ($query) => $query->where('status', 'shipped'))
+                ->icon('heroicon-m-truck'),
+            'Delivered' => Tab::make()->query(fn ($query) => $query->where('status', 'delivered'))
+                ->icon('heroicon-m-check-badge'),
+            'Cancelled' => Tab::make()->query(fn ($query) => $query->where('status', 'cancelled'))
+                ->icon('heroicon-m-x-circle'),
         ];
     }
 }
